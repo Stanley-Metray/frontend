188be519daa09523591c582d0f0e5901315032ea
Stanley Metray <stanleymetray@gmail.com>
2024-12-15 13:05:05 +0530

    first example of react components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+const footerStyle = {
+    height : '10vh',
+}
+
+const Footer = ()=>{
+    return <>
+        <footer className='text-white bg-dark d-flex flex-column justify-content-center align-items-center' style={footerStyle}>
+            This is the Footer component
+        </footer>
+    </>
+}
+
+export default Footer;
