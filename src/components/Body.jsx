17d8516cf7142b018b4e5f2d0750aa7e6a2d7ba0
Stanley Metray <stanleymetray@gmail.com>
2024-12-17 20:03:25 +0530

    implementation of react useEffect hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,33 @@
-// import Products from "./Products";
-// import Time from "./Time";
-// import LoginForm from "./LoginForm";
-import One from "./One";
-// const bodyStyle = {
-//     height : '100vh',
-// }
+import { useEffect, useState } from "react";
 
-const Body = ()=>{
+const Body = () => {
+
+    let [count, setCount] = useState(1);
+    let [data, setData] = useState(null);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const result = await response.json();
+            setData(result);
+        }
+
+        fetchData();
+    }, []);
+
     return <>
-        <One/>
+        <div className="d-flex flex-column h-50 justify-content-center align-items-center gap-3 p-3">
+
+            <h1>{count}</h1>
+            <button onClick={increment} className="btn btn-danger">Increment</button>
+            <ol>
+                {data && data.map(user => <li key={user.id}>{user.name}</li>)}
+            </ol>
+        </div>
     </>
 }
 
