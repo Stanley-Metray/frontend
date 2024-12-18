73ea8ea7dc718863c1a8f045c10fd732e0ab0e0b
Stanley Metray <stanleymetray@gmail.com>
2024-12-18 17:03:39 +0530

    Usage of browser router

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,33 +1,22 @@
-import { useEffect, useState } from "react";
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 const Body = () => {
 
-    let [count, setCount] = useState(1);
-    let [data, setData] = useState(null);
-
-    const increment = () => {
-        setCount(count + 1);
-    }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            const result = await response.json();
-            setData(result);
-        }
-
-        fetchData();
-    }, []);
-
     return <>
-        <div className="d-flex flex-column h-50 justify-content-center align-items-center gap-3 p-3">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+        </Routes>
 
-            <h1>{count}</h1>
-            <button onClick={increment} className="btn btn-danger">Increment</button>
-            <ol>
-                {data && data.map(user => <li key={user.id}>{user.name}</li>)}
-            </ol>
-        </div>
+        <ol>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+        </ol>
     </>
 }
 
