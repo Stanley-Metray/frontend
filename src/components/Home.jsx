9399d3f26f8b5d643667a179c56af959656ec717
Stanley Metray <stanleymetray@gmail.com>
2024-12-24 13:10:34 +0530

    usage of forwardRef in react

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, forwardRef } from "react";
 
 const Home = () => {
     const inputRef = useRef();
@@ -7,12 +7,23 @@ const Home = () => {
         inputRef.current.focus();
     }, []);
 
+    const handleClick = ()=>{
+        console.log(inputRef.current.value);
+    }
+
     return <>
         <h1>This is Home Page</h1>
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <input ref={inputRef} type="text" className="form-control w-25" placeholder="Type" />
+        <div className="d-flex flex-column justify-content-center align-items-center gap-2">
+            <Input ref={inputRef} placeholder="Search" />
+            <button onClick={handleClick} className="btn btn-primary">Submit</button>
         </div>
     </>
 }
+
+const Input = forwardRef(( props, ref)=>{
+        return <>
+            <input ref={ref} type="text" className="form-control w-25" placeholder={props.placeholder} />
+        </>
+});
 
 export default Home;
