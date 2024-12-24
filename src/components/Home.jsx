151fd7473bf976c2ab79378c845b9dd599ea927e
Stanley Metray <stanleymetray@gmail.com>
2024-12-24 12:37:22 +0530

    understanding useRef in react

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,17 @@
-const Home = ()=>{
+import { useEffect, useRef } from "react";
+
+const Home = () => {
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+
     return <>
         <h1>This is Home Page</h1>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+            <input ref={inputRef} type="text" className="form-control w-25" placeholder="Type" />
+        </div>
     </>
 }
 
